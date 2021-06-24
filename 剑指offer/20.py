541cfd7f51fba62bc17558be0934fd43d2cdb19f
tsir-mac <841227297@qq.com>
2021-06-24 17:40:16 +0800

    剑指 Offer 20. 表示数值的字符串

---
+++ 剑指offer/20.py
@@ -0,0 +1,124 @@
+"""
+解题思路:
+这道题使用有限状态自动机，自动机驱动的编程，可以被看做一种暴力枚举方法的延伸：它穷尽了在任何一种情况下，对应任何的输入，需要做的事情。
+
+我们首先定义出自动机的状态集合，那么怎么挖掘出所有可能的状态呢？一个常用的技巧是，用「当前处理到字符串的哪个部分」当作状态的表述
+
+1. 起始的空格
+2. 符号位
+3. 整数部分
+4. 左侧有整数的小数点
+5. 左侧无整数的小数点(根据第二条额外规则，需要对左侧有无整数的两种小数点做区分)
+6. 小数部分
+7. 字符e｜E
+8. 指数部分的符号位
+9. 指数部分的整数部分
+10. 末尾的空格
+
+下一步是找出「初始状态」和「接受状态」的集合。根据题意，「初始状态」应当为状态 1，而「接受状态」的集合则为状态 3、状态 4、状态 6、状态 9 以及状态 10。
+换言之，字符串的末尾要么是空格，要么是数字，要么是小数点，但前提是小数点的前面有数字。
+
+最后，需要定义「转移规则」。结合数值字符串应当具备的格式，将自动机转移的过程以图解的方式表示出来
+
+"""
+from enum import Enum
+
+
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        State = Enum("State", [
+            "STATE_INITIAL",
+            "STATE_INT_SIGN",
+            "STATE_INTEGER",
+            "STATE_POINT",
+            "STATE_POINT_WITHOUT_INT",
+            "STATE_FRACTION",
+            "STATE_EXP",
+            "STATE_EXP_SIGN",
+            "STATE_EXP_NUMBER",
+            "STATE_END"
+        ])
+        Chartype = Enum("Chartype", [
+            "CHAR_NUMBER",
+            "CHAR_EXP",
+            "CHAR_POINT",
+            "CHAR_SIGN",
+            "CHAR_SPACE",
+            "CHAR_ILLEGAL"
+        ])
+
+        def toChartype(ch: str) -> Chartype:
+            if ch.isdigit():
+                return Chartype.CHAR_NUMBER
+            elif ch.lower() == "e":
+                return Chartype.CHAR_EXP
+            elif ch == ".":
+                return Chartype.CHAR_POINT
+            elif ch == "+" or ch == "-":
+                return Chartype.CHAR_SIGN
+            elif ch == " ":
+                return Chartype.CHAR_SPACE
+            else:
+                return Chartype.CHAR_ILLEGAL
+
+        transfer = {
+            State.STATE_INITIAL: {
+                Chartype.CHAR_SPACE: State.STATE_INITIAL,
+                Chartype.CHAR_NUMBER: State.STATE_INTEGER,
+                Chartype.CHAR_POINT: State.STATE_POINT_WITHOUT_INT,
+                Chartype.CHAR_SIGN: State.STATE_INT_SIGN
+            },
+            State.STATE_INT_SIGN: {
+                Chartype.CHAR_NUMBER: State.STATE_INTEGER,
+                Chartype.CHAR_POINT: State.STATE_POINT_WITHOUT_INT
+            },
+            State.STATE_INTEGER: {
+                Chartype.CHAR_NUMBER: State.STATE_INTEGER,
+                Chartype.CHAR_EXP: State.STATE_EXP,
+                Chartype.CHAR_POINT: State.STATE_POINT,
+                Chartype.CHAR_SPACE: State.STATE_END
+            },
+            State.STATE_POINT: {
+                Chartype.CHAR_NUMBER: State.STATE_FRACTION,
+                Chartype.CHAR_EXP: State.STATE_EXP,
+                Chartype.CHAR_SPACE: State.STATE_END
+            },
+            State.STATE_POINT_WITHOUT_INT: {
+                Chartype.CHAR_NUMBER: State.STATE_FRACTION
+            },
+            State.STATE_FRACTION: {
+                Chartype.CHAR_NUMBER: State.STATE_FRACTION,
+                Chartype.CHAR_EXP: State.STATE_EXP,
+                Chartype.CHAR_SPACE: State.STATE_END
+            },
+            State.STATE_EXP: {
+                Chartype.CHAR_NUMBER: State.STATE_EXP_NUMBER,
+                Chartype.CHAR_SIGN: State.STATE_EXP_SIGN
+            },
+            State.STATE_EXP_SIGN: {
+                Chartype.CHAR_NUMBER: State.STATE_EXP_NUMBER
+            },
+            State.STATE_EXP_NUMBER: {
+                Chartype.CHAR_NUMBER: State.STATE_EXP_NUMBER,
+                Chartype.CHAR_SPACE: State.STATE_END
+            },
+            State.STATE_END: {
+                Chartype.CHAR_SPACE: State.STATE_END
+            },
+        }
+
+        st = State.STATE_INITIAL
+        for ch in s:
+            typ = toChartype(ch)
+            if typ not in transfer[st]:
+                return False
+            st = transfer[st][typ]
+
+        return st in [State.STATE_INTEGER, State.STATE_POINT, State.STATE_FRACTION, State.STATE_EXP_NUMBER,
+                      State.STATE_END]
+
+
+
+if __name__ == '__main__':
+    print(Solution().isNumber(" 2.3"))
+
