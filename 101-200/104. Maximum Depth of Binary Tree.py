aebd9db1700abbfd0ded0b08e6d70d4a678e310e
tsir-mac <841227297@qq.com>
2021-06-30 10:47:08 +0800

    104. 二叉树的最大深度

---
+++ 101-200/104. Maximum Depth of Binary Tree.py
@@ -0,0 +1,22 @@
+"""
+解题思路:
+要求一个二叉树的最大深度，那其实自顶向下分析，对于根节点来说
+f(n) = 1 + max(f(n.left), f(n.right)), 那我们就可以使用递归来实现
+
+"""
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
